d6c1b7c54ede44ce453a8788af3535c898f92fe1
hamza38954017 <hamza38954017@gmail.com>
2026-03-25 07:31:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ FIREBASE_DB_URL = "https://xhamster-70a9b-default-rtdb.firebaseio.com"
 BASE_DOMAIN = "https://xhamster45.desi"
 
 # Limits & Bounds
-VIDEO_CONCURRENCY_LIMIT = 30
+VIDEO_CONCURRENCY_LIMIT = 5
 START_ID = 1
 END_ID = 22875
 BATCH_SIZE = 1000
